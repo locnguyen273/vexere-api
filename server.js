6a3d1d8b1835436200dc9332bbed8d508be140df
Loc Nguyen Janeto <nguyentanloc@teams.airlab.dev>
2024-01-12 09:37:20 +0700

    init repo

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const { sequelize } = require("./models");
+const app = express();
+
+// cài đặt ứng dụng sử dụng kiểu json
+app.use(express.json());
+
+// cài đặt static file
+const publicPathDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicPathDirectory));
+
+// listen event connect
+app.listen(3000, async () => {
+  console.log("App listening on http://localhost:3000");
+  try {
+    await sequelize.authenticate();
+    console.log("connected");
+  } catch (error) {
+    console.error("error", error);
+  }
+});
